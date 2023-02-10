804bd3edc0bc9c397b59211d66b53a6a626cc946
ChaitanyaKottha <chythanya.k@gmail.com>
2023-02-10 05:54:00 +0530

    updating roboshop-jenkins-shared-library

--- vars/terraform.groovy
+++ vars/terraform.groovy
@@ -5,5 +5,12 @@ def call() {
               label 'workstation'
           }
       }
+      parameters {
+          string(name: 'INFRA_ENV', defaultvalue: '', description: 'Enter env like dev or prod')
+      }
+
+      stages {
+          stage ()
+      }
   }
 }
