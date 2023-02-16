844ca2ecab1d4178b302060d2f1936670537d4c0
ChaitanyaKottha <chythanya.k@gmail.com>
2023-02-17 01:59:02 +0530

    updating roboshop-jenkins-shared-library

--- vars/common.groovy
+++ vars/common.groovy
@@ -38,7 +38,7 @@ def artifactPush() {
         sh "zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile"
     }
     if (app_lang == 'maven') {
-        sh "zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION"
+        sh "zip -r ${component}-${TAG_NAME}.zip ${component}.jar VERSION ${extraFiles}"
     }
     //sh 'ls -l'
     NEXUS_USER = sh ( script: 'aws ssm get-parameters --region us-east-1 --names nexus.user  --with-decryption --query Parameters[0].Value | sed \'s/"//g\'', returnStdout: true).trim()
